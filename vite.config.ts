2e87acef45dbe532a299fa01fd925ef51340a4b2
n0xgg04 <dulieu.vblc@gmail.com>
2023-09-13 11:15:25 +0700

    add: env port

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
+    server: {
+        port: Number(process.env.REACTAPP_PORT),
+    },
     plugins: [react()],
     resolve: {
         alias: {
